a05267405aa46afde4e8420c581fca7b9f49a020
ouhyeon <163230719+savre01@users.noreply.github.com>
2024-05-08 06:24:07 +0000

    끝

---
+++ seed.js
@@ -0,0 +1,87 @@
+// seed.js
+"use strict";
+
+const { CommandStartedEvent } = require("mongodb");
+/**
+ * Listing 15.9 (p. 224)
+ * 새로운 데이터 생성
+ */
+// 모듈 가져오기
+const mongoose = require("mongoose"),
+    Subscriber = require("./models/Subscriber");
+
+// 데이터베이스 연결 설정
+mongoose.connect(
+    "mongodb+srv://ouhyeun40:Xa4RrHYgGze9ML5P@ut-node.dezf20b.mongodb.net/?retryWrites=true&w=majority&appName=UT-Node"
+  );
+mongoose.connection;
+
+// subscribers 배열 생성 (5개 이상)
+var subscribers = [
+  {
+    name: "Hulk",
+    email: "a@a.com",
+    newsletter: "true",
+  },
+  {
+    name: "Spider",
+    email: "a@b.com",
+    newsletter: "false",
+  },
+  {
+    name: "Wizard",
+    email: "a@c.com",
+    newsletter: "true",
+  },
+  {
+    name: "Hawkeye",
+    email: "a@d.com",
+    newsletter: "false",
+  },
+  {
+    name: "Iron Main",
+    email: "a@e.com",
+    newsletter: "true",
+  }
+];
+
+// 기존 데이터 제거
+/*
+Subscriber
+    .deleteMany({})
+    .exec()
+    .then(result => {
+        console.log(`Deleted : ${result.deletedCount} records.`) ;       
+    })
+    .catch(error => {
+        console.log(`Error: ${error.message}`);
+    });
+*/
+
+var commands = [];
+
+
+// 프라미스 생성을 위한 구독자 객체 루프
+subscribers.forEach(s => {
+    commands.push(
+        Subscriber
+        .create({
+            name: s.name,
+            email: s.email,
+            newsletter: s.newsletter
+        })
+        .then(s =>{
+            console.log(`Created: ${s.name}`);
+        })
+    );
+});
+
+// 프라미스 생성 후 로깅 작업
+Promise.all(commands)
+    .then(r => {
+        console.log(JSON.stringify(r, null, 2));
+        mongoose.connection.close();
+    })
+    .catch(e => {
+        console.log(e);
+    });
